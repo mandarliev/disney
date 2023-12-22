ccb753e51d2a720f053d3bdf186775d7eeca56f8
Dimitar <dimitar.mandarliev@gmail.com>
2023-12-22 23:16:43 +0200

    Building the MovieCarousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import MoviesCarousel from "@/components/MoviesCarousel";
+import {
+  getPopularMovies,
+  getTopRatedMovies,
+  getUpcomingMovies,
+} from "@/lib/getMovies";
 
-export default function Home() {
+export default async function Home() {
+  const upcomingMovies = await getUpcomingMovies();
+  const topRatedMovies = await getTopRatedMovies();
+  const popularMovies = await getPopularMovies();
+
   return (
     <main className="">
-      <h1>Hello</h1>
+      {/* <CarouselBannerWrapper />*/}
+
+      <div className="flex flex-col space-y-2 xl:-mt-48">
+        <MoviesCarousel movies={upcomingMovies} title="Upcoming" />
+        <MoviesCarousel movies={topRatedMovies} title="Top Rated" />
+        <MoviesCarousel movies={popularMovies} title="Popular" />
+      </div>
     </main>
   );
 }
--- next.config.js
+++ next.config.js
@@ -4,7 +4,11 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "links.papareact.com",
+        hostname: "links.papareact.com",  
+      },
+      {
+        protocol: "http",
+        hostname: "image.tmdb.org"
       },
     ],
   },
