8eb72d80f6de247b9778611178af5baf7b159cfc
Dimitar <dimitar.mandarliev@gmail.com>
2023-12-23 19:17:58 +0200

    Added the CarouselBanner

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import CarouselBannerWrapper from "@/components/CarouselBannerWrapper";
 import MoviesCarousel from "@/components/MoviesCarousel";
 import {
   getPopularMovies,
@@ -12,7 +13,7 @@ export default async function Home() {
 
   return (
     <main className="">
-      {/* <CarouselBannerWrapper />*/}
+      <CarouselBannerWrapper/>
 
       <div className="flex flex-col space-y-2 xl:-mt-48">
         <MoviesCarousel movies={upcomingMovies} title="Upcoming" />
