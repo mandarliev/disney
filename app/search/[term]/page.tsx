b5141d04db1699fc021683576084cd16cdd827df
Dimitar <dimitar.mandarliev@gmail.com>
2023-12-23 23:26:23 +0200

    Created an API and added the Azure Function functionality

--- app/search/[term]/page.tsx
+++ app/search/[term]/page.tsx
@@ -1,3 +1,4 @@
+import AISuggestion from "@/components/AISuggestion";
 import MoviesCarousel from "@/components/MoviesCarousel";
 import { getPopularMovies, getSearchedMovies } from "@/lib/getMovies";
 import { notFound } from "next/navigation";
@@ -24,7 +25,7 @@ async function SearchPage({ params: { term } }: Props) {
       <div className="flex flex-col space-y-5 mt-32 xl:mt-42">
         <h1 className="text-6xl font-bold px-10">Results for {termToUse}</h1>
 
-        {/* AI suggestion */}
+        <AISuggestion term={termToUse} />
 
         <MoviesCarousel title="Movies" movies={movies} isVertical />
         <MoviesCarousel title="Popular Movies" movies={popularMovies} />
--- function/src/functions/getAISuggestion.ts
+++ function/src/functions/getAISuggestion.ts
@@ -1,15 +1,44 @@
-import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import {
+  app,
+  HttpRequest,
+  HttpResponseInit,
+  InvocationContext,
+} from "@azure/functions";
 
-export async function getAISuggestion(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    context.log(`Http function processed request for url "${request.url}"`);
+import OpenAI from "openai";
 
-    const name = request.query.get('name') || await request.text() || 'world';
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
-    return { body: `Hello, ${name}!` };
-};
+export async function getAISuggestion(
+  request: HttpRequest,
+  context: InvocationContext
+): Promise<HttpResponseInit> {
+  context.log(`Http function processed request for url "${request.url}"`);
+  const term = request.query.get("term");
 
-app.http('getAISuggestion', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: getAISuggestion
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: `You are a digital video assistant working for services such as Netflix, Disney Plus & Amazon Prime Video. Your job is to provide suggestions based on the videos the user specifies. Provide an quirky breakdown of what the user should watch next! It should only list the names of the films after the introduction. Keep the response short and sweet! Always list at least 3 films as suggestions. If the user mentions a genre, you should provide a suggestion based on that genre.`,
+      },
+      {
+        role: "user",
+        content: `I like: ${term}`,
+      },
+    ],
+    model: "gpt-3.5-turbo",
+  });
+
+  console.log(completion.choices[0]);
+
+  return { body: completion.choices[0].message.content || "No Suggestion" };
+}
+
+app.http("getAISuggestion", {
+  methods: ["GET"],
+  authLevel: "anonymous",
+  handler: getAISuggestion,
 });
