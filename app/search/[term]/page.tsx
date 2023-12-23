349e8d7bfbba361a6a475396430cb0a60ee5b224
Dimitar <dimitar.mandarliev@gmail.com>
2023-12-23 19:35:04 +0200

    Added the Genre page

--- app/search/[term]/page.tsx
+++ app/search/[term]/page.tsx
@@ -1,3 +1,5 @@
+import MoviesCarousel from "@/components/MoviesCarousel";
+import { getPopularMovies, getSearchedMovies } from "@/lib/getMovies";
 import { notFound } from "next/navigation";
 
 type Props = {
@@ -6,15 +8,29 @@ type Props = {
   };
 };
 
-function SearchPage({ params: { term } }: Props) {
+async function SearchPage({ params: { term } }: Props) {
   if (!term) notFound();
 
   const termToUse = decodeURI(term);
 
   // API call to get the Searched Movies
-  // API call to get the Popular Movies
+  const movies = await getSearchedMovies(termToUse);
 
-  return <div>Welcome to the SearchPage {termToUse}</div>;
+  // API call to get the Popular Movies
+  const popularMovies = await getPopularMovies();
+
+  return (
+    <div className="max-w-7xl mx-auto">
+      <div className="flex flex-col space-y-5 mt-32 xl:mt-42">
+        <h1 className="text-6xl font-bold px-10">Results for {termToUse}</h1>
+
+        {/* AI suggestion */}
+
+        <MoviesCarousel title="Movies" movies={movies} isVertical />
+        <MoviesCarousel title="Popular Movies" movies={popularMovies} />
+      </div>
+    </div>
+  );
 }
 
 export default SearchPage;
